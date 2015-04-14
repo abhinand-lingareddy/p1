5f492d25b486dd58f3659e87575beca74893fa7b
KH1858 <abhinand.lingareddy@kony.com>
2015-04-14 21:59:27 +0530

    adding php 5.6

--- scripts/installer.bat
+++ scripts/installer.bat
@@ -8,6 +8,8 @@ SET FLYWAY_FOLDER_PATH=%ROOT_FOLDER_PATH%\Public\flyway-2.3
 SET FLYWAY_ZIP_PATH=flyway.zip
 SET APACHE_FOLDER_PATH=%ROOT_FOLDER_PATH%\Public\Apache24
 SET APACHE_ZIP_PATH=apache.zip
+SET PHP_FOLDER_PATH=%ROOT_FOLDER_PATH%\Public\php
+SET PHP_ZIP_PATH=php5.6.zip
 
 REM -----commenting extraction
 IF NOT EXIST "%MYSQL_HOME%" (
@@ -25,8 +27,15 @@ REM -----commenting extraction
 IF NOT EXIST "%APACHE_FOLDER_PATH%" (
 	echo %APACHE_FOLDER_PATH% NOT FOUND so extracting it
 	call "%ProgramFiles%\7-Zip\7z" x "%SERVERS_FOLDER_NAME%\%APACHE_ZIP_PATH%" -o"%SERVERS_FOLDER_NAME%\"
-	cscript %ROOT_FOLDER_PATH%\scripts\replace.vbs "%APACHE_FOLDER_PATH%\conf\httpd.conf" "c:/Apache24" "%APACHE_FOLDER_PATH%"
+	cscript %ROOT_FOLDER_PATH%\scripts\replace.vbs "%APACHE_FOLDER_PATH%\conf\httpd.conf" "c:/" "%SERVERS_FOLDER_NAME%/"
 ) 
+
+REM -----commenting extraction
+IF NOT EXIST "%PHP_FOLDER_PATH%" (
+	echo %PHP_FOLDER_PATH% NOT FOUND so extracting it
+	call "%ProgramFiles%\7-Zip\7z" x "%SERVERS_FOLDER_NAME%\%PHP_ZIP_PATH%" -o"%SERVERS_FOLDER_NAME%\"
+) 
+
 
 
 
@@ -43,6 +52,8 @@ REM migrating SQL scripts
 call "%FLYWAY_FOLDER_PATH%\flyway" -url=jdbc:mysql://localhost:3307/connections -user=root -locations=filesystem:%ROOT_FOLDER_PATH%/sqlscripts/flyway migrate
 REM checking status
 call "%FLYWAY_FOLDER_PATH%\flyway" -url=jdbc:mysql://localhost:3307/connections -user=root info
+
+set PATH=%PATH%;%PHP_FOLDER_PATH%
 
 start call "%APACHE_FOLDER_PATH%\bin\httpd"
 
