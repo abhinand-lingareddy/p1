67b85b512f26a0f99d4d3fe6c58b4c4516990e98
KH1858 <abhinand.lingareddy@kony.com>
2015-04-18 00:20:50 +0530

    changes as part of correcting constants usage

--- src/dbconstants.php
+++ src/dbconstants.php
@@ -1,8 +1,8 @@
 <?php
-define(servername, "localhost:3307");
-define(username, "root");
-define(dbpassword, NULL);
-define(dbname, "connections");
+define("servername", "localhost:3307");
+define("username", "root");
+define("dbpassword", NULL);
+define("dbname", "connections");
 
 
 ?>
